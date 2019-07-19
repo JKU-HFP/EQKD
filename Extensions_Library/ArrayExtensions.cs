80895f4bd25756c216962fd6273a8fec64c8ea18
Schimpf-Christian <christian.schimpf@jku.at>
2019-07-19 10:09:57 +0200

    Add Array Concat

---
+++ Extensions_Library/ArrayExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extensions_Library
+{
+    public static class ArrayExtensions
+    {
+        public static T[] ConcatArray<T>(this T[] inarray, T[] addarray)
+        {
+            var outarr = new T[inarray.Length + addarray.Length];
+            inarray.CopyTo(outarr, 0);
+            addarray.CopyTo(outarr, inarray.Length);
+            return outarr;
+        }
+    }
+}
