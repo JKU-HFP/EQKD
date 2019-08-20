1324f3abadf5cedea6d3699cc36a06ec758191a7
Schimpf-Christian <christian.schimpf@jku.at>
2019-08-20 12:41:38 +0200

    Stokes measurement

--- Entanglement_Library/Stokes.cs
+++ Entanglement_Library/Stokes.cs
@@ -6,23 +6,40 @@ using System.Threading.Tasks;
 using Thorlabs.TLPM_64.Interop;
 using System.Runtime.InteropServices;
 using Extensions_Library;
+using Stage_Library;
+using System.IO;
 
 namespace Entanglement_Library
 {
     public class Stokes
     {
+        //#################################################
+        //##  P R O P E R T I E S
+        //#################################################
 
+        public bool IsConnected { get; set; } = false;
+
+
+        //#################################################
+        //##  P R I V A T E S
+        //#################################################
 
         private Action<string> _loggerCallback;
-        private TLPM tlpm;
+        private TLPM _powermeter;
+        private IRotationStage _rotStage;
 
-        public Stokes(Action<string> loggercallback)
+        public Stokes(Action<string> loggercallback, IRotationStage rotStage)
         {
-
             _loggerCallback = loggercallback;
+            _rotStage = rotStage;
+        }
 
-            //Test Thorlabs
-                       
+        public void Connect()
+        {
+            double powerValue;
+
+            //Connect to Thorlabs
+
             try
             {
                 HandleRef Instrument_Handle = new HandleRef();
@@ -55,10 +72,12 @@ namespace Entanglement_Library
                     return;
                 }
 
-                tlpm = new TLPM(firstPowermeterFound, false, false);  //  For valid Ressource_Name see NI-Visa documentation.
-                double powerValue;
-                int err = tlpm.measPower(out powerValue);
+                _powermeter = new TLPM(firstPowermeterFound, false, false);  //  For valid Ressource_Name see NI-Visa documentation.
+
+                int err = _powermeter.measPower(out powerValue);
                 WriteLog("powerValue.ToString()");
+
+                IsConnected = true;
             }
             catch (BadImageFormatException bie)
             {
@@ -72,14 +91,29 @@ namespace Entanglement_Library
             {
                 WriteLog(ex.Message);
             }
-            finally
+        }
+
+        public double GetPower()
+        {
+            if(!IsConnected)
             {
-                if (tlpm != null)
-                    tlpm.Dispose();
+                WriteLog("Powermeter not connected");
+                return -1.0;
             }
 
+            double power;
+            int err = _powermeter.measPower(out power);
+            return power;
+        }
 
-            ///////
+        public void GetStokes(string filename = "Stokes.txt")
+        {
+            //Scan 360 degree
+            for (int pos=0; pos<360; pos=pos+2)
+            {
+                _rotStage.Move_Absolute(pos);
+                File.AppendAllLines(filename, new string[] { $"{pos:F2}\t{GetPower()}" });
+            }
         }
 
         private void WriteLog(string message)
